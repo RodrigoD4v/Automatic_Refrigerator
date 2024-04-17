c1f21976786e19ca4bd5e9281edf5e32ce9eb4cf
KayoLeanndro <kayoleanndro2@gmail.com>
2024-04-17 00:41:26 -0300

    initial setting

---
+++ src/main/java/com/automatica/geladeira/GeladeiraApplication.java
@@ -0,0 +1,13 @@
+package com.automatica.geladeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeladeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeladeiraApplication.class, args);
+	}
+
+}
